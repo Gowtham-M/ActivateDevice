e17d62db5aa487b4b27f6940f6f77e09fb565f72
Gowtham <chmgowtham@gmail.com>
2024-04-19 00:36:25 +0530

    added dialer to buttons

--- myapp/src/components/endpage.js
+++ myapp/src/components/endpage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // Define a functional component named MyComponent
 const Endpage = () => {
+    const phoneNumber = '1234567890';
     console.log()
   return (
     <div>
@@ -12,7 +13,7 @@ const Endpage = () => {
       <div style={{fontSize:"30px", textAlign:"center", marginBottom:"70px", marginTop:"70px"}}>Click above to call for instant details</div>
       <div style={{ borderBottom: '3px solid black', width:"700px", marginLeft:'29%' }}></div>
       <div style={{fontSize:"40px", textAlign:"center", marginTop:"70px", marginBottom:"70px"}}>Activate your device without key</div>
-      <div style={{marginLeft:"42%"}}><div style={{fontSize:"30px",fontStyle:"bold", textAlign:"center", width:"250px",height:"50px", backgroundColor:"skyblue", fontWeight:"bold"}}>8054199332</div></div>
+      <div style={{marginLeft:"42%"}}><div style={{fontSize:"30px",fontStyle:"bold", textAlign:"center", width:"250px",height:"50px", backgroundColor:"skyblue", fontWeight:"bold"}}><a href={`tel:${phoneNumber}`}>8054199332</a></div></div>
     </div>
   );
 };
